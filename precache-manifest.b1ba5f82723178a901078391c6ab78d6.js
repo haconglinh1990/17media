574c516f4a16eced52060fe131596927e9d92558
linhhc <linhhc@abivin.com>
2019-07-23 17:33:18 +0700

    Updates

---
+++ precache-manifest.b1ba5f82723178a901078391c6ab78d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c75e360f7c2d0e9df622d49c290c1f1",
+    "url": "/haconglinh1990/17media/index.html"
+  },
+  {
+    "revision": "9c0184b4051f2ec1f355",
+    "url": "/haconglinh1990/17media/static/js/2.98002b2c.chunk.js"
+  },
+  {
+    "revision": "0badc8bcbc7187aaa103",
+    "url": "/haconglinh1990/17media/static/js/main.b5dba5bb.chunk.js"
+  },
+  {
+    "revision": "71d705b6aa357f596aaa",
+    "url": "/haconglinh1990/17media/static/js/runtime~main.78e00229.js"
+  }
+]);
